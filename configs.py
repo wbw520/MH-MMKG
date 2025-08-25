2d35cb95715531b1379f89157e63f77e2895416b
wbw520 <bowen.wang@is.ids.osaka-u.ac.jp>
2025-08-25 17:19:47 +0900

    update to camera ready version.

--- configs.py
+++ configs.py
@@ -5,6 +5,6 @@ parser.add_argument('--dataset_dir', type=str, default="")
 parser.add_argument('--mode', type=str, default="Online Vision-needed", choices=["all", "Direct Vision-needed", "Direct", "Perfect", "self Search", "Offline", "Online Vision-needed"],
                     help='the order (Direct Vision-needed to Online Vision-needed) corresponding to the experiment setting in the ICCV 2025 paper.')
 parser.add_argument('--model_name', type=str, default="gpt-4o-2024-11-20")
-parser.add_argument('--api_key', type=str, default="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")
+parser.add_argument('--api_key', type=str, default="")
 parser.add_argument('--use_name', type=bool, default=True, help='whether use monster name for input query.')
 parser.add_argument('--use_extra', type=bool, default=True, help='whether use extra data for input query.')
